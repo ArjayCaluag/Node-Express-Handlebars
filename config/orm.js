5ad9dd041d277c3a1b22f28c4cb8d10184ef51c7
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-31 20:03:40 -0800

    Create initial empty orm.js file

--- config/orm.js
+++ config/orm.js
@@ -3,5 +3,4 @@ const connection = require('./connection.js');
 // const selectAll()
 // const insertOne()
 // const updateOne()
-
 module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,6 @@
+const orm = require('../config/orm.js')
+
+// Create code that will call the ORM functions
+
+
+modules.exports = burgers;
