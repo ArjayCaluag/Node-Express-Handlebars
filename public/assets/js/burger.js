cfbdd09352b1a621296b1005d1b5bcba3a478343
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-02 17:44:33 -0800

    Create burger.js public file

---
+++ public/assets/js/burger.js
@@ -0,0 +1,80 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (event) {
+      console.info('DOM loaded');
+    }
+  
+    // UPDATE
+    const changeBurgerBtns = document.querySelectorAll('.change-burger');
+  
+    // Set up the event listener for the create button
+    if (changeBurgerBtns) {
+      changeBurgerBtns.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          // Grabs the id of the element that goes by the name, "id"
+          const id = e.target.getAttribute('data-id');
+          const newBurger = e.target.getAttribute('data-newburger');
+  
+          const newBurgerState = {
+            devoured: newBurger,
+          };
+  
+          fetch(`/api/burgers/${id}`, {
+            method: 'PUT',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+  
+            // make sure to serialize the JSON body
+            body: JSON.stringify(newBurgerState),
+          }).then((response) => {
+            // Check that the response is all good
+            // Reload the page so the user can see the new quote
+            if (response.ok) {
+              console.log(`changed sleep to: ${newBurger}`);
+              location.reload('/');
+            } else {
+              alert('something went wrong!');
+            }
+          });
+        });
+      });
+    }
+  
+    // CREATE
+    const createCatBtn = document.getElementById('create-form');
+  
+    if (createCatBtn) {
+      createCatBtn.addEventListener('submit', (e) => {
+        e.preventDefault();
+  
+        // Grabs the value of the textarea that goes by the name, "quote"
+        const newBurger = {
+          burger_name: document.getElementById('ca').value.trim(),
+          // devoured: document.getElementById('not-devoured').checked,
+        };
+  
+        // Send POST request to create a new quote
+        fetch('/api/burgers', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+  
+          // make sure to serialize the JSON body
+          body: JSON.stringify(newBurger),
+        }).then(() => {
+          // Empty the form
+          document.getElementById('ca').value = '';
+  
+          // Reload the page so the user can see the new quote
+          console.log('Created a new burger!');
+          location.reload();
+        });
+      });
+    }
+  });
+  
